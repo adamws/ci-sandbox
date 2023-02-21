934d4d192b4a9936bb3ac8f274ca3781deedbd93
adamws <adamws@users.noreply.github.com>
2023-02-21 19:17:39 +0100

    progress

--- .circleci/use-kicad-python.ps1
+++ .circleci/use-kicad-python.ps1
@@ -1,12 +1,18 @@
+$pythonPath = "C:\scoop\apps\kicad\current\bin"
+
 Write-Output "Remove all Python paths from environment"
 $arrPath = $env:Path -split ";" | Where-Object {$_ -notMatch "Python"}
 
 Write-Output "Add KiCad's python to environment"
-$addPaths = "C:\scoop\apps\kicad\current\bin","C:\scoop\apps\kicad\current\bin\Scripts"
+$addPaths = $pythonPath,"$pythonPath\Scripts"
 $env:Path = ($arrPath + $addPaths) -join ";"
 
-python -c "import pcbnew; print('KiCad version:' + pcbnew.Version())"
+python -c "import pcbnew; print('KiCad version: ' + pcbnew.Version())"
 
 if ($LastExitCode -ne 0) {
   Write-Error "Could not import pcbnew"
 }
+
+Write-Output "Creating symbolic link for python3"
+New-Item -Path "$pythonPath\python3.exe" -ItemType SymbolicLink -Value "$pythonPath\python.exe" -Force
+
