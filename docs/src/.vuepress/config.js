05837c2b0466e6083231368c9f0738a1cd409172
adamws <adamws@users.noreply.github.com>
2023-03-02 19:54:16 +0100

    testing docs deployment

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,41 @@
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+
+export default defineUserConfig({
+  title: 'Keyboard Tools',
+  lang: 'en-US',
+  base: '/ci-sandbox/',
+  head: [['link', { rel: 'icon', href: '/ci-sandbox/images/favicon.ico' }]],
+  theme: defaultTheme({
+    home: "/",
+    navbar: [
+      {
+        text: 'Github',
+        link: 'https://github.com/adamws/keyboard-tools'
+      }
+    ],
+    sidebar: {
+      '/kicad-project-generator/': [
+        {
+          text: 'KiCad Project Generator',
+          collapsable: false,
+          children: [
+            {
+              text: 'Introduction',
+              link: 'README.md'
+            },
+            'features',
+            'examples',
+            'guide',
+            'development',
+          ]
+        }
+      ],
+    },
+    lastUpdated: false,
+    contributors: false,
+    nextLinks: true,
+    prevLinkgs: true,
+  }),
+})
+
---
+++ docs/src/index.md
@@ -0,0 +1,11 @@
+---
+home: true
+heroImage: /images/logo.png
+heroText: Keyboard Tools
+tagline: Tools for mechanical keyboard design
+actions:
+  - text: Quick Start →
+    link: /kicad-project-generator/
+footer: Made by adamws
+---
+
